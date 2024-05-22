401cad929443089b1b96b6799a4cbc64a4fba81f
sagar_varade <varade.sagar12@gmail.com>
2024-05-22 23:21:03 +0530

    3 Project Working

---
+++ Util/src/main/java/com/util/Token.java
@@ -0,0 +1,32 @@
+package com.util;
+
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class Token {
+	public Map<String, String> getDecompressToken(String token) throws JsonMappingException, JsonProcessingException {
+		String[] chunks = token.split("\\.");
+		Base64.Decoder decoder = Base64.getUrlDecoder();
+
+		String header = new String(decoder.decode(chunks[0]));
+		String payload = new String(decoder.decode(chunks[1]));
+		//System.out.println("Header :"+header);
+		//System.out.println("payload :"+payload);
+		Map<String,String> myMap = new HashMap<String, String>();
+		Map<String,String> myMap2 = new HashMap<String, String>();
+		ObjectMapper objectMapper = new ObjectMapper();
+		myMap = objectMapper.readValue(header, HashMap.class);
+		myMap2 = objectMapper.readValue(payload, HashMap.class);
+		//System.out.println("Map is: "+myMap);
+		//System.out.println("Map is: "+myMap2);
+		
+		return myMap2;
+	}
+}
+
