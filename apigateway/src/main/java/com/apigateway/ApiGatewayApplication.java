e725362aa02d3980110772277673f8dab806be2d
sagar_varade <varade.sagar12@gmail.com>
2024-07-03 21:50:38 +0530

    d

--- apigateway/src/main/java/com/apigateway/ApiGatewayApplication.java
+++ apigateway/src/main/java/com/apigateway/ApiGatewayApplication.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import io.netty.resolver.DefaultAddressResolverGroup;
 import reactor.netty.http.client.HttpClient;
 
+import java.net.http.HttpClient;
+
 @SpringBootApplication
 public class ApiGatewayApplication {
 
