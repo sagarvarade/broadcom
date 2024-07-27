65c8e2db1ee8701ead01ace5a328eab58a8d400f
sagar_varade <varade.sagar12@gmail.com>
2024-07-27 18:56:22 +0530

    auth update

---
+++ authentication/src/main/java/com/authentication/service/UserAuthService.java
@@ -0,0 +1,26 @@
+package com.authentication.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.authentication.entity.UserInfo;
+import com.authentication.repository.UserInfoRepository;
+
+
+@Service
+public class UserAuthService {
+
+	@Autowired
+	private UserInfoRepository repository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	public String addUser(UserInfo userInfo) {
+		userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
+		repository.save(userInfo);
+		return "BLogic added to system ";
+	}
+}
